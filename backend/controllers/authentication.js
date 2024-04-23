b320fe65a48db46ef27a6ab6fc7697f913732f07
MaximilianHolleran <mhholleran@gmail.com>
2024-04-23 19:53:47 -0400

    step 6 complete

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -6,13 +6,22 @@ const { User } = db
 
   
 router.post('/', async (req, res) => {
-
+    
     let user = await User.findOne({
-        where: { email: req.body.email }
+        where: { 
+            email: req.body.email          
+        }
     })
 
-    console.log(user)
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ 
+            message: `Could not find a user with the provided username and password` 
+        })
+    } else {
+        res.json({ user })
+    }
 })
+
   
 
 
