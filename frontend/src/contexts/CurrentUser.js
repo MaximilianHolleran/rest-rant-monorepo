c5323c933fec133bc2387beecde94437171d9281
MaximilianHolleran <mhholleran@gmail.com>
2024-04-25 19:16:53 -0400

    activity using jwts complete

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useEffect } from "react";
 
-
 export const CurrentUser = createContext()
 
 function CurrentUserProvider({ children }){
@@ -9,7 +8,11 @@ function CurrentUserProvider({ children }){
     useEffect(() => {
 
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
             let user = await response.json()
             setCurrentUser(user)
         }
